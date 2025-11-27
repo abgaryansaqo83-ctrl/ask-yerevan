9fc41747c815fac0e410e800c789352cb1afe3d2
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2025-11-27 04:28:12 +0100

    Update languages.py

--- backend/languages.py
+++ backend/languages.py
@@ -1,1 +1,10 @@
 
+def get_text(key: str, lang: str = "hy") -> str:
+    texts = {
+        "start": {
+            "hy": "Բարև՛, ես AskYerevan բոտն եմ 🙌\nԻնչի՞ կարիք ունես։",
+            "ru": "Привет! Я бот AskYerevan 🙌",
+            "en": "Hello! I’m AskYerevan bot 🙌"
+        }
+    }
+    return texts.get(key, {}).get(lang, key)
