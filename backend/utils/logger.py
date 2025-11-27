83d281cac906ceb583f86bc8eae604b621ed2dea
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2025-11-27 04:27:27 +0100

    Update logger.py

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,1 +1,11 @@
+import logging
+
+logger = logging.getLogger("askyerevan")
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
