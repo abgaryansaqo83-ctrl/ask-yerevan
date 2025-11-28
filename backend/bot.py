540c10f189d58b4fdc284f0311560f623a07d529
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2025-11-28 16:31:38 +0100

    Update bot.py

--- backend/bot.py
+++ backend/bot.py
@@ -1,4 +1,5 @@
 import asyncio
+from aiogram.client.default import DefaultBotProperties
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
@@ -9,7 +10,10 @@ from backend.utils.logger import logger
 from backend.languages import get_text
 
 
-bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=settings.BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
 dp = Dispatcher()
 
 
