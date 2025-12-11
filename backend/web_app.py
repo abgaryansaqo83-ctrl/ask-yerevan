9871e17da4503be55917d7842cfa4e4d13ba1ac4
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2025-12-11 19:07:16 +0100

    Update web_app.py

--- backend/web_app.py
+++ backend/web_app.py
@@ -1,25 +1,31 @@
+# backend/web_app.py
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from backend.utils.logger import logger
-
 app = FastAPI(title="AskYerevan Web")
-
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.on_event("startup")
-async def on_startup():
-    logger.info("🌐 AskYerevan Web Service started")
-
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/events", response_class=HTMLResponse)
+async def events_page(request: Request):
+    # ուզում ես՝ այստեղ կարող ես կպցնել get_events_by_category(...),
+    # չի ուզում՝ թող մաքուր ձեռքով գրված content լինի template-ում
+    return templates.TemplateResponse("events.html", {"request": request})
+
+@app.get("/blog", response_class=HTMLResponse)
+async def blog_page(request: Request):
+    return templates.TemplateResponse("blog.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about_page(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
 
 @app.get("/health")
 async def health():
