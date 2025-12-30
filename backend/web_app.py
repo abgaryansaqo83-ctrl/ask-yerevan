e940bec7a52ceb279de0abf0b7a99ddc1b4fa0b1
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2025-12-30 20:56:15 +0100

    Update web_app.py

--- backend/web_app.py
+++ backend/web_app.py
@@ -60,7 +60,7 @@ async def indexhy(request: Request):
         {
             "request": request,
             "lang": "hy",
-            "iswintertheme": iswinterthemeenabled(),
+            "iswintertheme": is_winter_theme_enabled(),
             "hero_events": hero_events,
             "hero_city": hero_city,
             "hero_culture": hero_culture,
@@ -78,7 +78,7 @@ async def indexen(request: Request):
         {
             "request": request,
             "lang": "en",
-            "iswintertheme": iswinterthemeenabled(),
+            "iswintertheme": is_winter_theme_enabled(),
             "hero_events": hero_events,
             "hero_city": hero_city,
             "hero_culture": hero_culture,
