318a173886abf676c159232cbe6cdb25040a6f8d
abgaryansaqo83-ctrl <abgaryansaqo83@gmail.com>
2026-02-03 12:02:26 +0100

    Update user_question.py

--- backend/bot/states/user_question.py
+++ backend/bot/states/user_question.py
@@ -1,9 +1,10 @@
-# backend/bot/states/question.py
+# backend/bot/states/user_question.py
 # ============================================
 #   FSM STATE — USER QUESTION
 # ============================================
 
 from aiogram.fsm.state import StatesGroup, State
+
 
 class UserQuestion(StatesGroup):
     waiting_for_question = State()
